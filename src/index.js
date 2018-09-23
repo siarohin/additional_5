d0576dcc92282554f630e885e1c2e19d894a577c
A.Seregin <a.siarohin@gmail.com>
2018-09-23 18:30:12 +0300

    #19

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-  //
+
+    var arr = [],
+    	result;
+
+    arr.push(str.charAt(0));
+    // console.log(arr);
+
+    for (var i = 1; i < str.length; i++) {        
+        result = true;
+        for (var symbol of bracketsConfig) {
+            if (arr[arr.length-1] === symbol[0] && str.charAt(i) === symbol[1]) {
+                arr.pop();
+                result = false; break;
+            }
+        }
+
+        if (result == true) {
+        	arr.push(str.charAt(i));
+        }
+
+    }
+
+    return arr.length === 0;
 }
